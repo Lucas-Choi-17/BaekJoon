35249ae0563d796a446e2e0c80087a460e3bb8d9
Choi Seung In <csissis@naver.com>
2022-01-12 23:49:17 +0900

    #2741
    printing 1 to N

---
+++ C_Lang_study/No_2741.c
@@ -0,0 +1,13 @@
+//
+// Created by 최승인 on 2022/01/12.
+//
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int i = 1; i <= N; ++i) {
+        printf("%d\n", i);
+    }
+}
