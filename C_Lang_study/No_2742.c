32614abcf2a19f228abb6da3f6e98900157384ed
Choi Seung In <csissis@naver.com>
2022-01-15 18:46:17 +0900

    #2742 - printing num from N to 1

---
+++ C_Lang_study/No_2742.c
@@ -0,0 +1,13 @@
+//
+// Created by 최승인 on 2022/01/15.
+//
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    for (int i = N; i > 0; i--) {
+        printf("%d\n", i);
+    }
+}
